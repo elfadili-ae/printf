d4194b9503075d8a71e208a712fcf05b80dac00d
elfadili-ae <elfadili.ae@gmail.com>
2023-04-21 11:15:48 +0000

    Script: added _printf.c & main.h

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _printf - similar function to printf
+ * @format: text to print
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int charCount = 0;
+
+
+	return (charCount);
+}
